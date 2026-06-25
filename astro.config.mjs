3179ad88cc167dd4b3faac4e8f01f5dc8b985f96
AxelResendizRzm <ivanramres@gmail.com>
2026-06-25 15:05:51 -0600

    chore: fijar versión de Node para corregir estilos de Tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import compress from "astro-compress";
 import tailwindcss from '@tailwindcss/vite';
-
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
@@ -19,5 +17,5 @@ export default defineConfig({
     }
   },
 
-  integrations: [sitemap(), compress()],
+  integrations: [sitemap()],
 });

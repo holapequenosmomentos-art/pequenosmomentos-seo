0b51d33e513223f7138e4d20fcf353f3f3ce29c5
AxelResendizRzm <ivanramres@gmail.com>
2026-06-25 15:19:27 -0600

    fix: styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,12 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import sitemap from '@astrojs/sitemap';
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'static',
+   adapter: cloudflare(),
   site: 'https://pequenosmomentos.mx',
   vite: {
     plugins: [tailwindcss()],
